c73bacfd53f53fcc47a0859edb261909795ecba7
AllieInJune04 <kyanhtrang0406@gmail.com>
2023-03-16 08:41:51 +0700

    fixing

--- final_project/lib/src/features/authentication/controllers/splash_screen_controller.dart
+++ final_project/lib/src/features/authentication/controllers/splash_screen_controller.dart
@@ -1,3 +1,4 @@
+import 'package:final_project/src/features/authentication/screens/on_boarding/on_boarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:final_project/src/features/authentication/controllers/splash_screen_controller.dart';
@@ -7,14 +8,12 @@ import '../screens/welcome/welcome_screen.dart';
 class SplashScreenController extends GetxController{
   static SplashScreenController get find => Get.find();
 
-
   RxBool animate = false.obs;
-
 
   Future startAnimation() async {
     await Future.delayed(const Duration(milliseconds: 500));
     animate.value = true;
     await Future.delayed(const Duration(milliseconds: 5000));
-    Get.to(const WelcomeScreen());
+    Get.to(const OnBoardingScreen());
   }
 }
