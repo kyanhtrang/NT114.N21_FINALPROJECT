d3a41901c11f30e409fd83009cf1fde9265828d0
AllieInJune04 <kyanhtrang0406@gmail.com>
2023-03-19 21:25:58 +0700

    add signup

--- final_project/lib/src/features/authentication/screens/signup/widgets/signup_form_widget.dart
+++ final_project/lib/src/features/authentication/screens/signup/widgets/signup_form_widget.dart
@@ -1,5 +1,8 @@
+import 'package:final_project/src/features/authentication/controllers/signup_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 import '../../../../../constants/sizes.dart';
 import '../../../../../constants/text_strings.dart';
@@ -11,47 +14,44 @@ class SignUpFormWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(SignUpController());
+    final _formKey = GlobalKey<FormState>();
+
     return Container(
       padding: const EdgeInsets.symmetric(vertical: tFormHeight - 10),
       child: Form(
+        key: _formKey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextFormField(
-              decoration: const InputDecoration(
-                  label: Text(tFullName),
-                  prefixIcon: Icon(Icons.person_outline_rounded)),
+              controller: controller.fullName,
+              decoration: const InputDecoration(label: Text(tFullName), prefixIcon: Icon(Icons.person_outline_rounded)),
             ),
             const SizedBox(height: tFormHeight - 20),
             TextFormField(
-              decoration: const InputDecoration(
-                  label: Text(tEmail),
-                  prefixIcon: Icon(Icons.email_outlined)),
+              controller: controller.email,
+              decoration: const InputDecoration(label: Text(tEmail), prefixIcon: Icon(Icons.email_outlined)),
             ),
             const SizedBox(height: tFormHeight - 20),
             TextFormField(
-              decoration: const InputDecoration(
-                  label: Text(tPhoneNo),
-                  prefixIcon: Icon(Icons.numbers)),
+              controller: controller.phoneNo,
+              decoration: const InputDecoration(label: Text(tPhoneNo), prefixIcon: Icon(Icons.numbers)),
             ),
-            const SizedBox(
-                height: tFormHeight - 20),
+            const SizedBox(height: tFormHeight - 20),
             TextFormField(
-              decoration: const InputDecoration(
-                  label: Text(tPassword),
-                  prefixIcon: Icon(Icons.fingerprint),
-                suffixIcon: IconButton(
-                  onPressed: null,
-                  icon: Icon(Icons.remove_red_eye_sharp),
-                ),
-              ),
+              controller: controller.password,
+              decoration: const InputDecoration(label: Text(tPassword), prefixIcon: Icon(Icons.fingerprint)),
             ),
-            const SizedBox(
-                height: tFormHeight - 10),
+            const SizedBox(height: tFormHeight - 10),
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  if(_formKey.currentState!.validate()){
+                    SignUpController.instance.registerUser(controller.email.text.trim(), controller.password.text.trim());
+                  }
+                },
                 child: Text(tSignup.toUpperCase()),
               ),
             )
