fc26782d9c9ec9f1d6b40cb24c299cdcc5223844
AllieInJune04 <kyanhtrang0406@gmail.com>
2023-03-21 14:40:27 +0700

    update phone forget password screen

--- final_project/lib/src/features/authentication/screens/forget_password/forget_password_phone/forget_pasword_phone_screen.dart
+++ final_project/lib/src/features/authentication/screens/forget_password/forget_password_phone/forget_pasword_phone_screen.dart
@@ -9,8 +9,8 @@ import '../../../../../constants/sizes.dart';
 import '../../../../../constants/text_strings.dart';
 import '../forget_password_otp/otp_screen.dart';
 
-class ForgetPasswordMailScreen extends StatelessWidget {
-  const ForgetPasswordMailScreen({Key? key}) : super(key: key);
+class ForgetPasswordPhoneScreen extends StatelessWidget {
+  const ForgetPasswordPhoneScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +25,7 @@ class ForgetPasswordMailScreen extends StatelessWidget {
                 FormHeaderWidget(
                   image: tForgetPasswordImage,
                   title: tForgetPassword.toUpperCase(),
-                  subTitle: tForgetMailSubTitle,
+                  subTitle: tForgetPhoneSubTitle,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   heightBetween: 30.0,
                   textAlign: TextAlign.center,
@@ -36,8 +36,8 @@ class ForgetPasswordMailScreen extends StatelessWidget {
                     children: [
                       TextFormField(
                         decoration: const InputDecoration(
-                            label: Text(tEmail),
-                            hintText: tEmail,
+                            label: Text(tPhoneNo),
+                            hintText: tPhoneNo,
                             prefixIcon: Icon(Icons.mail_outline_rounded)),
                       ),
                       const SizedBox(height: 20.0),
