2ee38ecfad4bea7ef650bc9336aa9c593e388485
AllieInJune04 <kyanhtrang0406@gmail.com>
2023-03-22 11:50:05 +0700

    finish test

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:final_project/firebase_options.dart';
 import 'package:final_project/src/features/authentication/screens/splash_screen/splash_screen.dart';
 import 'package:final_project/src/repository/authentication_repository/authentication_repository.dart';
 import 'package:final_project/src/utils/theme/theme.dart';
+import 'package:final_project/testmap.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -24,7 +25,7 @@ class App extends StatelessWidget {
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const SplashScreen(),
+      home: const MapSample(),
     );
   }
 }
