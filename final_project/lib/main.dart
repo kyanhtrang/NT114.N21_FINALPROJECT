e37f09bde2ee8078b6beccf7aba3a0ae810e873a
AllieInJune04 <kyanhtrang0406@gmail.com>
2023-03-30 21:18:45 +0700

    fixed

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:final_project/firebase_options.dart';
 import 'package:final_project/src/features/authentication/screens/login/login_screen.dart';
 import 'package:final_project/src/features/authentication/screens/splash_screen/splash_screen.dart';
 import 'package:final_project/src/features/authentication/screens/welcome/welcome_screen.dart';
@@ -7,13 +6,26 @@ import 'package:final_project/src/utils/theme/theme.dart';
 import 'package:final_project/testmap.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
-void main(){
+Future main() async {
+  // To load the .env file contents into dotenv.
+  // NOTE: fileName defaults to .env and can be omitted in this case.
+  // Ensure that the filename corresponds to the path in step 1 and 2.
+  await dotenv.load(fileName: ".env");
+  //...runapp
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp(
-      options:  DefaultFirebaseOptions.currentPlatform)
-      .then((value) => Get.put(AuthenticationRepository()));
+    options: FirebaseOptions(
+      apiKey: dotenv.get('API_KEY'),
+      authDomain: dotenv.get('AUTH_DOMAIN'),
+      projectId: dotenv.get('PROJECT_ID'),
+      storageBucket: dotenv.get('STORAGE_BUCKET'),
+      messagingSenderId: dotenv.get('MESSAGING_SENDER_ID'),
+      appId: dotenv.get('APP_ID'),
+    ),
+  );
   runApp(const App());
 }
 
