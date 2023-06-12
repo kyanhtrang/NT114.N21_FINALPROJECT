1c5ee7289abc5ddcb756c144ece14c8fc957b2f9
AshenTray <75878750+ashentray@users.noreply.github.com>
2023-06-12 14:34:33 +0700

    Revert "progress"
    This reverts commit e3821953950098afe29e54bd6e1b29bf0870cdff.

--- CustomerApp/app/src/main/java/com/example/customerapp/Fragment/SettingFragment.java
+++ CustomerApp/app/src/main/java/com/example/customerapp/Fragment/SettingFragment.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,22 +17,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingFragment extends Fragment {
     private View view;
+    private Button btnsignout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_setting, container, false);
-        view.findViewById(R.id.btn_profile).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*Fragment fragment =  new ProfileFragment();
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_container, fragment);
-                ft.addToBackStack(null);
-                ft.commit();*/
-            }
-        });
-        view.findViewById(R.id.logout_btn).setOnClickListener(new View.OnClickListener() {
+        btnsignout = (Button) view.findViewById(R.id.logout_btn);
+        btnsignout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 logout();
