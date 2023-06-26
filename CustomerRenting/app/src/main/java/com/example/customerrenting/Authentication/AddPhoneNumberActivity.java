5a562b2f55a7cf494a5bd3d9f11172906080c183
AllieInJune04 <kyanhtrang0406@gmail.com>
2023-06-27 01:00:33 +0700

    fixed

---
+++ CustomerRenting/app/src/main/java/com/example/customerrenting/Authentication/AddPhoneNumberActivity.java
@@ -0,0 +1,54 @@
+package com.example.customerrenting.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.customerrenting.R;
+
+public class AddPhoneNumberActivity extends AppCompatActivity {
+    TextView phone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_phone_number);
+
+        phone = (TextView) findViewById(R.id.input_phone);
+
+        phone.setText(getIntent().getStringExtra("phonenum"));
+        Intent intent = new Intent(AddPhoneNumberActivity.this, OTP.class);
+        findViewById(R.id.btn_continue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validatePhonenum(phone.getText().toString())) {
+                    intent.putExtra("phonenum", phone.getText().toString().trim());
+                    intent.putExtra("email", getIntent().getStringExtra("email"));
+                    intent.putExtra("password", getIntent().getStringExtra("password"));
+                    intent.putExtra("fullname", getIntent().getStringExtra("fullname"));
+                    intent.putExtra("gender", getIntent().getStringExtra("gender"));
+                    intent.putExtra("birth", getIntent().getStringExtra("birth"));
+                    Log.d("AddPhoneNumber", "This is a solid phone number");
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+    private boolean validatePhonenum(String val) {
+
+        if (val.isEmpty()) {
+            phone.setError("Hãy nhập số điện thoại");
+            return false;
+        } else if (val.length() < 9) {
+            phone.setError("Vui lòng nhập đúng định dạng số điện thoại!");
+            return false;
+        } else {
+            phone.setError(null);
+            return true;
+        }
+
+    }
+}
--- CustomerRenting/app/src/main/java/com/example/customerrenting/Fragments/SettingFragment.java
+++ CustomerRenting/app/src/main/java/com/example/customerrenting/Fragments/SettingFragment.java
@@ -1,5 +1,6 @@
 package com.example.customerrenting.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.customerrenting.Authentication.LoginActivity;
 import com.example.customerrenting.R;
+import com.example.customerrenting.User.UserProfileActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingFragment extends Fragment {
-
+    private View view;
+    private Button btnSignout, btnProfile;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        view = inflater.inflate(R.layout.fragment_setting, container, false);
+        btnSignout = (Button) view.findViewById(R.id.logout_btn);
+        btnProfile = (Button) view.findViewById(R.id.user_profile_btn) ;
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { profile(); }
+        });
+        btnSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+        return view;
+    }
+    public void logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void profile() {
+        Intent intent = new Intent(getActivity(), UserProfileActivity.class);
+        startActivity(intent);
     }
 }
