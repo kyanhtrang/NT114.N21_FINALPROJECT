49874c80e529c9db044356e078d94b61fa8a581e
HangLe23 <20521286@gm.uit.edu.vn>
2023-07-07 12:32:32 +0700

    update setting UI

--- CustomerRenting/app/src/main/java/com/example/customerrenting/Fragments/SettingFragment.java
+++ CustomerRenting/app/src/main/java/com/example/customerrenting/Fragments/SettingFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.customerrenting.Services.Authentication.LoginActivity;
 import com.example.customerrenting.R;
@@ -17,14 +18,15 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingFragment extends Fragment {
     private View view;
-    private Button btnSignout, btnProfile;
+    private Button btnSignout;
+    TextView  btnProfile;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_setting, container, false);
-        btnSignout = (Button) view.findViewById(R.id.logout_btn);
-        btnProfile = (Button) view.findViewById(R.id.user_profile_btn) ;
+        btnSignout = (Button) view.findViewById(R.id.logout);
+        btnProfile = (TextView) view.findViewById(R.id.accountSettings) ;
         btnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { profile(); }
